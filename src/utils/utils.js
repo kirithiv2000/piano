f82b497321aa863056fd31b96e98ebcf5c7857da
komalahire <komala@navgurukul.org>
2022-10-02 16:22:00 +0530

    remove frist arr ele

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,25 @@
 // create a list
 // create a function to remove the last index 
 // create a function to randomly generate a nested list with 4 boolean values.
+
+
+let nestedArr = [[ true,false, false, false],
+[ fasle, false, false, true],
+[ fasle, true, false, false],
+[ fasle, false, false, true],
+[ fasle, true, false, false],
+[ fasle, false, false, true],
+[ fasle, true, false, false],
+]
+
+function removeLastIndex(){
+    nestedArr.splice(0, 1); //arr.splice(index,1)
+}
+
+function randomlyGenerateList(){
+
+}
+
+
+
+ 
